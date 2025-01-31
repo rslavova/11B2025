62be5e7e0c58ebd7f4611280adf43a48e274b0d6
rslavova <r.slavova@pmg-pernik.com>
2025-01-31 11:33:20 +0200

    Initial commit

---
+++ MyFisrWebApp/MyFisrWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFisrWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
